a4264430cfd239fe71b6945ee337d35f1c2c7213
alfianh <alfian08.ah@gmail.com>
2016-04-04 12:05:03 +0700

    project

---
+++ app/src/main/java/dev/alfianh/fileuploadapp/MainActivity.java
@@ -0,0 +1,194 @@
+package dev.alfianh.fileuploadapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    private Button buttonChoose;
+    private Button buttonUpload;
+
+    private ImageView imageView;
+
+    private EditText editTextName;
+
+    private Bitmap bitmap;
+
+    private int PICK_IMAGE_REQUEST = 1;
+
+    private String UPLOAD_URL ="http://alfianh.koding.io/fjb/v1/products";
+
+    private String KEY_IMAGE = "image";
+    private String KEY_NAME = "name";
+    private String KEY_PRICE = "price";
+    private String KEY_STOCK = "stock";
+    private String KEY_DESC = "desc";
+    private String KEY_CATEGORY_ID = "category_id";
+
+
+    public EditText inpProductName;
+    public EditText inpDesc;
+    public EditText inpStock;
+    public EditText inpPrice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonChoose = (Button) findViewById(R.id.buttonChoose);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
+
+        editTextName = (EditText) findViewById(R.id.editText);
+
+        imageView  = (ImageView) findViewById(R.id.imageView);
+
+        inpProductName = (EditText) findViewById(R.id.inpProductName);
+        inpDesc = (EditText) findViewById(R.id.inpDesc);
+        inpStock = (EditText) findViewById(R.id.inpStock);
+        inpPrice = (EditText) findViewById(R.id.inpPrice);
+
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage(){
+        //Showing the progress dialog
+        final ProgressDialog loading = ProgressDialog.show(this,"Uploading...","Please wait...",false,false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Disimissing the progress dialog
+                        loading.dismiss();
+                        //Showing toast message of the response
+                        Toast.makeText(MainActivity.this, s , Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //Dismissing the progress dialog
+                        loading.dismiss();
+                        if(volleyError.networkResponse != null && volleyError.networkResponse.data != null){
+                            VolleyError error = new VolleyError(new String(volleyError.networkResponse.data));
+                            volleyError = error;
+                        }
+                        //Showing toast
+                        Log.e("ErrorVolley",volleyError.getMessage().toString() );
+                        Toast.makeText(MainActivity.this, volleyError.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "198deaaca4267bd553696ec166f50077");
+                return params;
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Converting Bitmap to String
+                String image = getStringImage(bitmap);
+
+                //Getting Image Name
+                String name = editTextName.getText().toString().trim();
+
+                //Creating parameters
+                Map<String,String> params = new Hashtable<String, String>();
+
+                String produkName = inpProductName.getText().toString();
+                String strDesc = inpDesc.getText().toString();
+                String strStock = inpStock.getText().toString();
+                String strPrice = inpPrice.getText().toString();
+                //Adding parameters
+                params.put(KEY_PRICE, strPrice);
+                params.put(KEY_STOCK, strStock);
+                params.put(KEY_DESC, strDesc);
+                params.put(KEY_CATEGORY_ID, "1");
+                params.put(KEY_IMAGE, image);
+                params.put(KEY_NAME, produkName);
+
+                //returning parameters
+                return params;
+            }
+        };
+
+        //Creating a Request Queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Adding request to the queue
+        requestQueue.add(stringRequest);
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Getting the Bitmap from Gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Setting the Bitmap to ImageView
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == buttonChoose){
+            showFileChooser();
+        }
+
+        if(v == buttonUpload){
+            uploadImage();
+        }
+    }
+}
